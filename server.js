6ca791126b59db59135ebce8f49bce2c722af3b0
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-11-09 13:42:12 -0800

    added README.md to repo

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 require('./lib/client').connect();
 
+const express = require('express');
+const cors = require('cors');
+const request = require('superagent');
 const app = require('./lib/app');
-
 const PORT = process.env.PORT || 7890;
+
+app.use(cors());
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
